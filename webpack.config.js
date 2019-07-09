0f2ee16bdf383bd752b08047d9b7ebd5e428ed90
dionesb <diones.breda@gmail.com>
2019-07-09 20:49:56 -0300

    Adding image import.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: 'file-loader'
       }
     ]
   }
