a54f0fddbd7a00c563b6ca0361a0ca28b28cd6dd
dionesb <diones.breda@gmail.com>
2019-07-09 22:05:03 -0300

    Adding config to Class Components

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,10 @@ module.exports = {
   presets: [
     /* Converte as funcionalidades do Javascript que o navegador 
     ainda não interpreta. Ex.: Import/Exports, Arow Functions, Classes etc. */
-    "@babel/preset-env",
+    '@babel/preset-env',
     /* Converte a funcionalidades do React que o navegador ainda não 
     interpreta. Ex.: JSX */
-    "@babel/preset-react",
-  ]
-}
+    '@babel/preset-react'
+  ],
+  plugins: ['@babel/plugin-proposal-class-properties']
+};
---
+++ src/components/TechList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  /* Estado do componente */
+  state = {
+    techs: ['Node.js', 'React.js', 'React Native']
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>React.js</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
